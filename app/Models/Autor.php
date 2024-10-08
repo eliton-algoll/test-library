b8ee0c3a4e2a28f4cdebefdbdcf3fabc1cd49d69
Eliton Mendes de Sousa <eliton@algoll.com.br>
2024-10-07 21:17:22 -0300

    feat: add models

---
+++ app/Models/Autor.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $CodAu
+ * @property string $nome
+ */
+class Autor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'Autor';
+
+    public function livros()
+    {
+        return $this->belongsToMany(Livro::class, 'Livro_Autor', 'CodAu', 'CodL');
+    }
+}
